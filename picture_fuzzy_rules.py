9cdd5b5b18c4a38f262dfba79d1df4ee1a5b08b9
Hung Phan Bao <hunghusthedspi@gmail.com>
2019-09-27 17:05:37 +0700

    update

--- picture_fuzzy_rules.py
+++ picture_fuzzy_rules.py
@@ -4,6 +4,8 @@ import operator
 import math
 from picture_fuzzy_clustering import *
 from cropper import *
+from readImage import readImage
+import skfuzzy as fuzz
 
 datas, matrixs = cropper()
 
@@ -21,7 +23,11 @@ if 0 == 0:
     # cal new cluster_center
     new_cluster_center = calculateClusterCenter(matrix, data, n1, k1)
 
-    defuzz = np.zeros((k1,3))
+    A = np.zeros((k1,3))
+    C = np.zeros((k1,3))
+    B = np.zeros((k1,3))
+    A_ = np.zeros((k1,3))
+    C_ = np.zeros((k1,3))
     # cal triangular picture fuzzy number of fuzzy rules
     for j in range(k1):
         u_matrix = matrix[:,j,0]
@@ -44,6 +50,27 @@ if 0 == 0:
             result = np.where(data >= b[k] )
             indexs = result[0]
             c[k] = np.sum(u[indexs] * color[indexs]) / np.sum(u[indexs])
-        defuzz[j] = (1*a_ + 2*a + 3*b + 2*c + 1*c_) / (1+2+3+2+1)
+        A_[j] = a_
+        C_[j] = c_
+        B[j] = b
+        A[j] = a
+        C[j] = c
+    defuzz = (1*A_ + 2*A + 3*B + 2*C + 1*C_) / (1+2+3+2+1)
+    # print(defuzz)
 
-    print(defuzz)
+# x = np.arange(11)
+# mfx = fuzz.trimf(x,[0,5,10])
+# print()
+# def triangular_matrix(x, a, b, c):
+#     if 
+
+input_data = readImage('images_test/8A0000.jpg')
+n = input_data.shape[0] 
+
+x = np.repeat(input_data, k1, axis= 0)
+a_matrix = np.vstack([A] * n)
+b_matrix = np.vstack([B] * n)
+c_matrix = np.vstack([C] * n)
+
+print(x.shape, a_matrix.shape)
+
--- test.py
+++ test.py
@@ -2,11 +2,13 @@ import sys
 sys.path.append('/usr/local/lib/python3.7/site-packages')
 import csv
 import numpy as np
-n = 512*384
-k = 8
-matrix = np.zeros((n*k,3))
-with open('results/membership_matrixs/8C0189 (3).csv') as f:
-    readCSV = csv.reader(f, delimiter = ',')
-    matrix = np.array(list(readCSV)).astype("float")
-    
-print(matrix.reshape((384,512,k,3)).shape)
+import skfuzzy as fuzz
+
+
+x = np.arange(30)
+x = x.reshape((5,3,2))
+
+# print(np.where(x>=5, x, np.zeros((5,3,2))))
+# print(np.where(x<=12, x, np.zeros((5,3,2))))
+print(x>=5)
+print(x<=12)
